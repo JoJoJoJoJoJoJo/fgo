451370a2119b89270e77dcf243e7a3bce4f3223f
JoJoJoJoJoJoJo <whr428@163.com>
2017-10-11 00:54:56 +0800

    add url and complete token checker

--- fgo/auth/views.py
+++ fgo/auth/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render
 from django.conf import settings
 import hashlib
+from django.http import HttpResponse
 
 
 def token_handler(request):
     token = settings.TOKEN
     try:
         data = request.GET
+        # print(data)
         if len(data) == 0:
-            return 'Error:No args received'
+            return HttpResponse('Error:No args received')
         # 发过来的请求带有4个参数
-        signature = data.get('signature')
-        timestamp  = data.get('timestamp')
-        nonce = data.get('nonce')
-        echostr = data.get('echostr')
-        #根据文档要求进行校验
+        signature = data['signature']
+        timestamp  = data['timestamp']
+        nonce = data['nonce']
+        echostr = data['echostr']
+        # 根据文档要求进行校验
         l = [token,timestamp,nonce]
         l.sort()
         sha1 = hashlib.sha1()
         map(sha1.update,l)
         hashcode = sha1.hexdigest()
         if hashcode == signature:
-            return echostr
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse(u'校验失败')
     except Exception as e:
-        return e
+        return HttpResponse('Internal Error,{}'.format(e))
 
