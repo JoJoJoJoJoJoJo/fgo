79effa83d9b1ba5ebe2b56cc8abf6df289f88ed4
JoJoJoJoJoJoJo <whr428@163.com>
2017-10-11 18:09:07 +0800

    fucking stupid error

--- fgo/auth/views.py
+++ fgo/auth/views.py
@@ -10,8 +10,7 @@ def token_handler(request):
     token = settings.TOKEN
     try:
         data = request.GET
-        print(data)
-        print(len(data))
+        # print(data)
         if len(data) == 0:
             return HttpResponse('Error:No args received')
         # 发过来的请求带有4个参数
@@ -19,22 +18,23 @@ def token_handler(request):
         timestamp = data['timestamp']
         nonce = data['nonce']
         echostr = data['echostr']
-        print(signature,timestamp,nonce,echostr)
+        # print(signature,timestamp,nonce,echostr)
         # 根据文档要求进行校验
         l = [token,timestamp,nonce]
         l.sort()
+        s = ''.join(l).encode()
         sha1 = hashlib.sha1()
-        map(sha1.update,l)
+        sha1.update(s)
         hashcode = sha1.hexdigest()
         if hashcode == signature:
-            print('success')
+            # print('success')
             return HttpResponse(echostr)
 
         else:
-            print('fail')
+            # print('fail')
             return HttpResponse(u'校验失败')
 
     except Exception as e:
-        print('error')
+        # print('error')
         return HttpResponse('Internal Error,{}'.format(e))
 
